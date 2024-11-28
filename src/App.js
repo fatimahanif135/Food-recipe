8bed4d6de55e4681f1a7edbd68b19b9b0413ce94
fatimahanif135 <fatimahanif332@gmail.com>
2024-11-28 18:43:59 +0500

    Initial commit with project setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Mainpage from './Component/Mainpage';
+import {  Route, Routes } from 'react-router-dom';
+import Mealinfo from './Component/Mealinfo';
+
+function App() {
+  return (
+    // <Mainpage/>
+<Routes>
+  <Route path='/' element={<Mainpage/>}/>
+   <Route path='/:mealid' element={<Mealinfo/>}/>
+
+</Routes>
+  );
+}
+
+export default App;
